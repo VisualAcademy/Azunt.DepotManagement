5ec79b3eb5e1c9a0b63a5a5ad91bddab8c003ff1
Park YongJune <redplus@redplus.net>
2025-04-30 02:32:22 +0900

    Initialize Azunt.DepotManagement project and implement DepotsTableBuilder to prepare Depots table

---
+++ src/Azunt.DepotManagement/Azunt.SqlServer/dbo/Tables/Depots.sql
@@ -0,0 +1,9 @@
+﻿--[0][0] 창고: Depots 
+CREATE TABLE [dbo].[Depots]
+(
+    [Id]        BIGINT             IDENTITY (1, 1) NOT NULL PRIMARY KEY,    -- 창고 고유 아이디, 자동 증가
+    [Active]    BIT                DEFAULT ((1)) NOT NULL,                  -- 활성 상태 표시, 기본값 1 (활성)
+    [CreatedAt] DATETIMEOFFSET (7) NOT NULL,                                -- 레코드 생성 시간
+    [CreatedBy] NVARCHAR (255)     NULL,                                    -- 레코드 생성자 이름
+    [Name]      NVARCHAR (MAX)     NULL                                     -- 이름
+);
---
+++ src/Azunt.DepotManagement/Azunt.Web/Azunt.Web/Program.cs
@@ -0,0 +1,78 @@
+using Azunt.DepotManagement;
+using Azunt.Web.Client.Pages;
+using Azunt.Web.Components;
+using Azunt.Web.Components.Account;
+using Azunt.Web.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents()
+    .AddAuthenticationStateSerialization();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+var app = builder.Build();
+
+// Depots 테이블 자동 생성 (마스터 DB만)
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DepotsTableBuilder.Run(services, forMaster: true); // or false for tenant DBs
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Azunt.Web.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
